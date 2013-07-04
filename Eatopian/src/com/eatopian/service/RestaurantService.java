71a0b45a85bdcfe9dd6486f4f6eeb2bf5dc7a319
Genie <genie@eatopian.com>
2013-07-04 11:46:49 -0400

    delete

--- Eatopian/src/com/eatopian/service/RestaurantService.java
+++
@@ -1,8 +0,0 @@
-package com.eatopian.service;
-
-import com.eatopian.entity.Restaurant;
-
-public interface RestaurantService {
-	void createRestaurant(Restaurant restaurant);
-	Restaurant getRestaurant(int id);
-}
