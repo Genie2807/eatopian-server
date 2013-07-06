dbee1cc2e731bacc28d45d2a616da209a42edb05
Genie <genie@eatopian.com>
2013-07-05 21:18:45 -0400

    add GFS test

--- Service/src/main/java/com/eatopian/dao/RestaurantUserDao.java
+++ Service/src/main/java/com/eatopian/dao/RestaurantUserDao.java
@@ -1,4 +1,8 @@
 package com.eatopian.dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -6,17 +10,28 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import com.eatopian.entity.Restaurant;
 import com.eatopian.entity.User;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSInputFile;
 
 
 public class RestaurantUserDao implements UserDao {
 	
 	private DataSource dataSource;
-	 
-
+	
+	
+	
+	public RestaurantUserDao(){
+		
+	}
+	
+	
 	public boolean login(User user) {
 		String checkSql = "SELECT * FROM Restaurant WHERE username = ? and password = ?";
 		Connection conn = null;
@@ -28,7 +43,7 @@ public class RestaurantUserDao implements UserDao {
 			ps.setString(2, user.getPassword());
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()){
-				user.setId((long)rs.getLong("id"));
+				user.setId(rs.getInt("id"));
 			}else{
 				rs.close();
 				ps.close();
@@ -86,8 +101,12 @@ public class RestaurantUserDao implements UserDao {
 			
 		return true;
 	}
+
 	
-	
+/*
+ *  mysql login and register test
+ */	
+ 
 //	public static void main( String[] args )
 //    {
 //    	ApplicationContext context = 
@@ -110,6 +129,65 @@ public class RestaurantUserDao implements UserDao {
 //		boolean z = rd.login(user);
 //		System.out.println(z);
 //    }
+
+
+/*
+ * GFS test	
+ */
+	
+//	public static void main(String[] args) {
+//		 
+//		try {
+// 
+//			Mongo mongo = new Mongo("g.sj.gs");
+//			DB db = mongo.getDB("GFS");
+////			DBCollection collection = db.getCollection("images");
+// 
+//			String newFileName = "dish1";
+//			
+//			
+//			File imageFile = new File("src/main/resources/dish1.png");
+// 
+//			// create image namespace
+//			GridFS GFS = new GridFS(db, "image");
+// 
+//			// get image file from local drive
+//			GridFSInputFile gfsFile = GFS.createFile(imageFile);
+// 
+//			// set a new filename for identify purpose
+//			gfsFile.setFilename(newFileName);
+// 
+//			// save the image file into mongoDB
+//			gfsFile.save();
+// 
+//			// print the result
+//			DBCursor cursor = GFS.getFileList();
+//			while (cursor.hasNext()) {
+//				System.out.println(cursor.next());
+//			}
+// 
+//			// get image file by it's filename
+//			GridFSDBFile imageForOutput = GFS.findOne(newFileName);
+// 
+//			// save it into a new image file
+//			imageForOutput.writeTo("src/main/resources/dish2.png");
+// 
+////			// remove the image file from mongoDB
+////			GFS.remove(GFS.findOne(newFileName));
+// 
+////			System.out.println("Done");
+// 
+//		} catch (UnknownHostException e) {
+//			e.printStackTrace();
+//		} catch (MongoException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+// 
+//	}
+	
+	
 	
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
